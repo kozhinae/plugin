2155dad22614eb38db3f85b71badeb71c1c3592c
Ekaterina Kozhina <katerinabonia@mail.ru>
2023-11-28 23:30:21 +0300

    Created class tgAction and implemented methods

--- src/main/java/tgAction.java
+++ src/main/java/tgAction.java
@@ -1,4 +1,10 @@
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class tgAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
 }
