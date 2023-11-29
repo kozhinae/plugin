84ee064d1d927498c95551a1ed23412b1c6aa41c
Ekaterina Kozhina <katerinabonia@mail.ru>
2023-11-29 22:50:57 +0300

    added nullable case

--- src/main/java/tgAction.java
+++ src/main/java/tgAction.java
@@ -1,7 +1,9 @@
+import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,9 +15,14 @@ public class tgAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent e) {
         @Nullable Editor editor = e.getData(PlatformDataKeys.EDITOR);
         String selectedText = editor.getSelectionModel().getSelectedText();
-        String encoded;
-        encoded = URLEncoder.encode(selectedText, StandardCharsets.UTF_8);
-        String url = String.format("https://t.me/katya_kozhina", encoded);
+        if (selectedText != null) {
+            String encoded;
+            encoded = URLEncoder.encode(selectedText, StandardCharsets.UTF_8);
+            String url = String.format("https://t.me/katya_kozhina", encoded);
+            BrowserUtil.browse(url);
+        } else {
+            Messages.showMessageDialog("Selection is empty, select code", "tg Action", Messages.getInformationIcon());
+        }
 
     }
 }
